86a6430f06e41ccae666945bed2c7bc0e365dc35
Anna <anytkin11@gmail.com>
2020-10-10 23:02:21 +0300

    Necessary files

---
+++ Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Anna Berezhniak on 09.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
